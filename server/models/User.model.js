3a5c254b8537b725e4b97f8b9321cb646625ddc3
shubhh004 <chaurasiashubh195@gmail.com>
2026-07-02 12:41:35 +0530

    Sprint 3C: User model

---
+++ server/models/User.model.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const SALT_ROUNDS = 10;
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, SALT_ROUNDS);
+  next();
+});
+
+userSchema.methods.comparePassword = function (candidate) {
+  return bcrypt.compare(candidate, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
